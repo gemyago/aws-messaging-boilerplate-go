8171828c27c06228fbbde11eac2881d82ae4303d
Eugene Myasyshchev <eugenemyasyshchev@gmail.com>
2024-11-23 09:46:48 +0100

    Boilerplate cleanup.

--- internal/api/http/server/server.go
+++ internal/api/http/server/server.go
@@ -7,7 +7,7 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/gemyago/golang-backend-boilerplate/internal/services"
+	"github.com/gemyago/aws-sqs-boilerplate-go/internal/services"
 	"go.uber.org/dig"
 )
 
