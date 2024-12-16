057d49fb286dfbbd7d84ddb434976e6a1ec77411
Eugene Myasyshchev <eugenemyasyshchev@gmail.com>
2024-12-16 21:43:34 +0100

    Fix var names.

--- deploy/terraform/variables.tf
+++ deploy/terraform/variables.tf
@@ -1,3 +1,12 @@
+variable "app_name" {
+  type = string
+}
+
 variable "aws_primary_region" {
   type = string
+}
+
+variable "ci_mode" {
+  type        = bool
+  description = "Value is true if running in CI mode. Should be used in very specific cases."
 }
