e5cdb323eb5269c0956394f519ee3fad8699e864
Eugene Myasyshchev <eugenemyasyshchev@gmail.com>
2024-12-19 11:19:23 +0100

    Passing detail as input.

--- deploy/terraform/modules/event-bus/main.tf
+++ deploy/terraform/modules/event-bus/main.tf
@@ -26,3 +26,19 @@ resource "aws_cloudwatch_event_rule" "custom_source_events" {
     detail-type = [ "myDetailType" ]
   })
 }
+
+resource "aws_cloudwatch_event_api_destination" "test" {
+  name                             = "api-destination"
+  description                      = "An API Destination"
+  invocation_endpoint              = "http://host.docker.internal:8080/messages/process"
+  http_method                      = "POST"
+  invocation_rate_limit_per_second = 20
+  connection_arn                   = aws_cloudwatch_event_connection.target_connection.arn
+}
+
+resource "aws_cloudwatch_event_target" "test_target" {
+  rule      = aws_cloudwatch_event_rule.custom_source_events.name
+  arn       = aws_cloudwatch_event_api_destination.test.arn
+  event_bus_name = aws_cloudwatch_event_bus.event_bus.name
+  input_path = "$.detail"
+}
