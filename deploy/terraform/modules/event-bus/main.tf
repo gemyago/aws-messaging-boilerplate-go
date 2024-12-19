cc3c298b809cf954a6ea9e2e0940d8a1d2e34264
Eugene Myasyshchev <eugenemyasyshchev@gmail.com>
2024-12-19 09:33:14 +0100

    Adding connection.

--- deploy/terraform/modules/event-bus/main.tf
+++ deploy/terraform/modules/event-bus/main.tf
@@ -2,3 +2,16 @@
 resource "aws_cloudwatch_event_bus" "event_bus" {
   name = var.bus_name
 }
+
+resource "aws_cloudwatch_event_connection" "target_connection" {
+  name               = "${var.bus_name}-target-connection"
+  description        = "Connection to the target"
+  authorization_type = "API_KEY"
+
+  auth_parameters {
+    api_key {
+      key   = "Authorization"
+      value = "Bearer NOT-USED"
+    }
+  }
+}
