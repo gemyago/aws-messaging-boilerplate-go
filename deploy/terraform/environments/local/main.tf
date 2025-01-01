77c302f6e756d282ac78eb0c11d70ba54921a0d7
Eugene Myasyshchev <eugenemyasyshchev@gmail.com>
2025-01-01 23:58:20 +0100

    Deploying test resources.

--- deploy/terraform/environments/local/main.tf
+++ deploy/terraform/environments/local/main.tf
@@ -15,3 +15,21 @@ module "api_service" {
   aws_primary_region       = var.aws_primary_region
   service_endpoint         = "http://localhost:8080"
 }
+
+# Below is required to run tests
+module "test_api_infra" {
+  source                = "../../modules/app-infra"
+  resources_prefix      = "${var.resources_prefix}test-"
+  resources_description = var.resources_description
+  local_stack_env       = true
+}
+module "test_api_service" {
+  source                   = "../../modules/api-service"
+  app_name                 = var.app_name
+  app_bus_name             = module.test_api_infra.app_bus_name
+  dummy_messages_topic_arn = module.test_api_infra.dummy_messages_topic_arn
+  resources_prefix         = "${var.resources_prefix}test-"
+  resources_description    = var.resources_description
+  aws_primary_region       = var.aws_primary_region
+  service_endpoint         = "http://localhost:8080" # TODO: Use a different port
+}
