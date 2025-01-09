95d661ffb2a9acc268314149997d43e93c9df562
Eugene Myasyshchev <eugenemyasyshchev@gmail.com>
2025-01-09 10:54:33 +0100

    Fix spelling.

--- deploy/terraform/modules/sns-sqs-subscriptions/outputs.tf
+++ deploy/terraform/modules/sns-sqs-subscriptions/outputs.tf
@@ -3,7 +3,7 @@ output "queues" {
     for index, subscription in local.sqs_subscriptions : {
       subscription_id = subscription.key
       url             = aws_sqs_queue.primary[subscription.key].url
-      sql_url         = aws_sqs_queue.dead_letter[subscription.key].url
+      dlq_url         = aws_sqs_queue.dead_letter[subscription.key].url
     }
   ]
 }
