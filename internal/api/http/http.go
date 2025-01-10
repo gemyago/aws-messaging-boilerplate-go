1a055024f14a11df560991087cc3290db3814216
Eugene Myasyshchev <eugenemyasyshchev@gmail.com>
2025-01-10 08:45:20 +0100

    Fix error handling.

--- internal/api/http/http.go
+++ internal/api/http/http.go
@@ -2,19 +2,37 @@ package http
 
 import (
 	"errors"
+	"log/slog"
+	"net/http"
 
 	"github.com/gemyago/aws-sqs-boilerplate-go/internal/api/http/server"
 	"github.com/gemyago/aws-sqs-boilerplate-go/internal/api/http/v1controllers"
 	"github.com/gemyago/aws-sqs-boilerplate-go/internal/api/http/v1routes/handlers"
 	"github.com/gemyago/aws-sqs-boilerplate-go/internal/di"
+	"github.com/gemyago/aws-sqs-boilerplate-go/internal/diag"
 	"go.uber.org/dig"
 )
 
 // Use apigen to generate v1routes
 //go:generate apigen ./v1schemas/api.yaml ./v1routes --verbose
 
-func NewV1RoutesApp(router *server.MuxRouterAdapter) *handlers.HTTPApp {
-	return handlers.NewHTTPApp(router)
+type V1RoutesAppDeps struct {
+	dig.In
+
+	RootLogger *slog.Logger
+
+	Router *server.MuxRouterAdapter
+}
+
+func NewV1RoutesApp(deps V1RoutesAppDeps) *handlers.HTTPApp {
+	logger := deps.RootLogger.WithGroup("http.v1routes")
+	return handlers.NewHTTPApp(deps.Router,
+		handlers.WithLogger(logger),
+		handlers.WithActionErrorHandler(func(r *http.Request, w http.ResponseWriter, err error) {
+			logger.ErrorContext(r.Context(), "Failed to process request", diag.ErrAttr(err))
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		}),
+	)
 }
 
 func Register(container *dig.Container) error {
